a8c92aba5c17c726594dae958159250d89a89836
louie_gomez <kinggomez027.lg@gmail.com>
2022-04-03 16:35:16 -0400

    commit tree 344b31ee87de99fe19306a7b87e02d54ea2983ac

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    const splitStringArray  = sentence.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    const joinedFunc = splitStringArray.join(" ")
+    return joinedFunc
+  })
 }
